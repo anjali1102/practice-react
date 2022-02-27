374ee87149e8d02f8f81cf62ba2ef3fc6600852b
Anjali Chauhan <56559378+anjali1102@users.noreply.github.com>
2022-02-27 23:33:28 +0530

    Conditional rendering
    try with "winter" and "anything" in line 31 in place of "summer"

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 const list =[1, 2, 3, 4, 5];
 
-function App({items}) {
+function Lake({name}){
   return (
     <div>
-      {items.map(item =>(
-        <li key={item.toString()}>{item}</li>
-      ))}
+      <h1>Visit {name}!</h1>
     </div>
   );
 }
 
-ReactDOM.render(<App items={list} />, document.getElementById("root"));
+function SkiResort({name}){
+  return(
+    <div>
+      <h1>Visit {name}!</h1>
+    </div>
+  );
+}
+
+function App(props){
+  if(props.season==="summer"){
+    return <Lake name="Jenny Lake" />;
+  }else if(props.season==="winter"){
+  return <SkiResort name="JHMR" /> 
+  }
+}
+
+
+
+ReactDOM.render(<App season="summer" />, document.getElementById("root"));
