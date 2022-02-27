de909e8203270f7474e27b32f228e26382870c5b
Anjali Chauhan <56559378+anjali1102@users.noreply.github.com>
2022-02-27 23:18:16 +0530

    object rendering

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const lakeList =[
-  {id:"1", name:"Echo", trailhead: "Echo"},
-  {id:"2", name:"Maud", trailhead: "Wrights"},
-  {id:"3", name:"Velma", trailhead: "Bayview"}
-];
+const list =[1, 2, 3, 4, 5];
 
-function App({lakes}) {
+function App({items}) {
   return (
     <div>
-      {lakes.map(lake =>(
-        <div>
-          <h2>{lake.name}</h2>
-          <p>Accessed by : {lake.trailhead}</p>
-        </div>
+      {items.map(item =>(
+        <li key={item.toString()}>{item}</li>
       ))}
     </div>
   );
 }
 
-ReactDOM.render(<App lakes={lakeList} />, document.getElementById("root"));
+ReactDOM.render(<App items={list} />, document.getElementById("root"));
